80d7c829498beb583aeb0ad931834b1affef13ea
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2022-05-30 23:55:38 -0700

    yes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource, marshal, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS, cross_origin
-from models import db
+from model.models import db
 from routes.player_info_routes import player_info
 from routes.player_basic_routes import player_basic
 from routes.player_advanced_routes import player_advanced
