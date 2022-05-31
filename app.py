5f25bf8b1a468bb74b7740e89adc241a65529f5f
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2022-05-31 00:57:18 -0700

    yes

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from flask_restful import Api, Resource, marshal, reqparse, abort, fields, marsh
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS, cross_origin
 from model.models import db
-from routes.player_info_routes import player_info
-from routes.player_basic_routes import player_basic
-from routes.player_advanced_routes import player_advanced
-from routes.player_analysis_routes import player_analysis
-from routes.player_get_stats_routes import player_get_stats
-from routes.team_info_routes import team_info
-from routes.team_stats_routes import team_stats
+from routes.player_info import player_info
+from routes.player_basic import player_basic
+from routes.player_advanced import player_advanced
+from routes.player_analysis import player_analysis
+from routes.player_get import player_get_stats
+from routes.team_info import team_info
+from routes.team_stats import team_stats
+from routes.team_get import team_get
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -24,6 +25,7 @@ app.register_blueprint(player_analysis)
 app.register_blueprint(player_get_stats)
 app.register_blueprint(team_info)
 app.register_blueprint(team_stats)
+app.register_blueprint(team_get)
 
 
 if '__main__' == __name__:
