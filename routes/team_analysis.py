415bcfc0e9bd1c96a0fba541d63bf67e9e18f43d
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2022-06-02 00:31:39 -0700

    featured teams

--- routes/team_analysis.py
+++ routes/team_analysis.py
@@ -4,32 +4,3 @@ from flask_restful import Api, Resource, marshal, reqparse, abort, fields, marsh
 from model.models import db, FeaturedTeams
 
 team_analysis = Blueprint('team_analysis', __name__)
-
-# post featured teams
-
-
-@team_analysis.route('/teams/featured_teams', methods=['POST'])
-@cross_origin()
-def post_featured_teams():
-    data = request.get_json()
-    featured_teams = FeaturedTeams(featured_offense_id=data['featured_offense_id'],
-                                   featured_defense_id=data['featured_defense_id'], featured_overall_id=data['featured_overall_id'])
-    db.session.add(featured_teams)
-    db.session.commit()
-    return jsonify({'message': 'Featured Teams added successfully'})
-
-
-# get featured teams
-
-
-@team_analysis.route('/teams/featured_teams', methods=['GET'])
-@cross_origin()
-def post_featured_teams():
-    featured_teams = FeaturedTeams.query.all()
-    featured_teams = featured_teams[-1]
-    featured_teams_dict = {}
-    featured_teams_dict['featured_offense_id'] = featured_teams.featured_offense_id
-    featured_teams_dict['featured_defense_id'] = featured_teams.featured_defense_id
-    featured_teams_dict['featured_overall_id'] = featured_teams.featured_overall_id
-    response = jsonify(featured_teams_dict)
-    return response
