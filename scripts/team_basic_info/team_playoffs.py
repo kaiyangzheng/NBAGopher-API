41a16ec6b958a5d554af3a42da10d4c5cc335347
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2022-06-06 19:16:27 -0700

    yes

--- scripts/team_basic_info/team_playoffs.py
+++ scripts/team_basic_info/team_playoffs.py
@@ -26,5 +26,87 @@ def get_playoffs_data():
     return table
 
 
-playoffs_data = get_playoffs_data()
-playoffs_data.to_csv('scripts/team_basic_info/playoffs_data.csv')
+def get_team_id(name):
+    team_info = requests.get(
+        'https://nbagopher-api.herokuapp.com/team/info')
+    team_info = team_info.json()
+    for key in team_info:
+        if team_info[key]['name'] == name:
+            return key
+    return -1
+
+
+def get_series_info(s):
+    team1_wins = s.split('\xa0')[1].replace(
+        '(', '').replace(')', '').split('-')[0]
+    team2_wins = s.split('\xa0')[1].replace(
+        '(', '').replace(')', '').split('-')[1]
+
+    s = s.split(' \xa0')[0]
+    keywords = [' trail ', ' lead ', ' tied with ', ' over ']
+    for word in keywords:
+        if word in s:
+            s = s.split(word)
+    team1_name = s[0]
+    team2_name = s[1]
+
+    team1_id = get_team_id(team1_name)
+    team2_id = get_team_id(team2_name)
+
+    return {'team1_id': team1_id, 'team1_wins': team1_wins, 'team2_id': team2_id, 'team2_wins': team2_wins}
+
+
+def main():
+    finals = []
+    east_final = []
+    west_final = []
+    east_semis = []
+    west_semis = []
+    east_first = []
+    west_first = []
+    playoffs_data = get_playoffs_data()
+    for index, row in playoffs_data.iterrows():
+        if (row[0] == 'Finals'):
+            finals.append(row[1])
+        elif (row[0] == 'Eastern Conference Finals'):
+            east_final.append(row[1])
+        elif (row[0] == 'Western Conference Finals'):
+            west_final.append(row[1])
+        elif (row[0] == 'Eastern Conference Semifinals'):
+            east_semis.append(row[1])
+        elif (row[0] == 'Western Conference Semifinals'):
+            west_semis.append(row[1])
+        elif (row[0] == 'Eastern Conference First Round'):
+            east_first.append(row[1])
+        elif (row[0] == 'Western Conference First Round'):
+            west_first.append(row[1])
+
+    for i in range(len(finals)):
+        finals[i] = get_series_info(finals[i])
+
+    for i in range(len(east_final)):
+        east_final[i] = get_series_info(east_final[i])
+        west_final[i] = get_series_info(west_final[i])
+
+    for i in range(len(east_semis)):
+        east_semis[i] = get_series_info(east_semis[i])
+        west_semis[i] = get_series_info(west_semis[i])
+
+    for i in range(len(east_first)):
+        east_first[i] = get_series_info(east_first[i])
+        west_first[i] = get_series_info(west_first[i])
+
+    data = {}
+    data['west_first'] = west_first
+    data['east_first'] = east_first
+    data['west_semi'] = west_semis
+    data['east_semi'] = east_semis
+    data['west_final'] = west_final
+    data['east_final'] = east_final
+    data['nba_final'] = finals
+
+    data = json.dumps(data)
+
+
+if __name__ == "__main__":
+    main()
